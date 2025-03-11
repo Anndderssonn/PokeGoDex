844f4f6ddf2b3f5770605b1dfa71aa68b743ce23
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-03-10 20:07:00 -0500

    feat: Add get pokemon service and create a request

---
+++ PokeGoDex/Services/FetchPokemon.swift
@@ -0,0 +1,29 @@
+//
+//  FetchPokemon.swift
+//  PokeGoDex
+//
+//  Created by Andersson on 10/03/25.
+//
+
+import Foundation
+
+struct FetchPokemon {
+    enum FetchError: Error {
+        case badResponse
+    }
+    
+    private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    
+    func getPokemon(_ id: Int) async throws -> PokemonEntity {
+        let fetchURL = baseURL.appending(path: String(id))
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let pokemon = try decoder.decode(PokemonEntity.self, from: data)
+        print(pokemon.name.capitalized)
+        return pokemon
+    }
+}
