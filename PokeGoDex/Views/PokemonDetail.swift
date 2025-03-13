c8cbed6f44707be8a2e14838c93c53ac1dc426d5
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-03-12 20:56:43 -0500

    feat: Add Image, title and stats in DetailView

---
+++ PokeGoDex/Views/PokemonDetail.swift
@@ -0,0 +1,73 @@
+//
+//  PokemonDetail.swift
+//  PokeGoDex
+//
+//  Created by Andersson on 12/03/25.
+//
+
+import SwiftUI
+
+struct PokemonDetail: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @EnvironmentObject private var pokemon: Pokemon
+    @State private var showShiny = false
+    
+    var body: some View {
+        ScrollView {
+            ZStack {
+                Image(pokemon.background)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: .black, radius: 6)
+                AsyncImage(url: pokemon.sprite) { image in
+                    image
+                        .interpolation(.none)
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 70)
+                        .shadow(color: .black, radius: 6)
+                } placeholder: {
+                    ProgressView()
+                }
+            }
+            HStack {
+                ForEach(pokemon.types!, id: \.self) { type in
+                    Text(type.capitalized)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.black)
+                        .shadow(color: .white, radius: 1)
+                        .padding(.vertical, 7)
+                        .padding(.horizontal)
+                        .background(Color(type.capitalized))
+                        .clipShape(.capsule)
+                }
+                Spacer()
+                Button {
+                    pokemon.favorite.toggle()
+                    do {
+                        try viewContext.save()
+                    } catch {
+                        print(error)
+                    }
+                } label: {
+                    Image(systemName: pokemon.favorite ? "star.fill" : "star")
+                        .font(.largeTitle)
+                        .tint(.yellow)
+                }
+            }
+            .padding()
+            Text("Stats")
+                .font(.title)
+            Stats(pokemon: pokemon)
+        }
+        .navigationTitle(pokemon.name!.capitalized)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        PokemonDetail()
+            .environmentObject(PersistenceController.previewPokemon)
+    }
+}
